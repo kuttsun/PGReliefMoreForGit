648ff1a6f104de34d1c377f8c6f0a071695a035e
kuttsun <tsukushimail+github@gmail.com>
2017-11-22 19:09:39 +0900

    コマンドライン引数の解析処理のベース作成と自動アップデート処理の不具合修正

--- PGReliefMoreForGit/App.xaml.cs
+++ PGReliefMoreForGit/App.xaml.cs
@@ -7,35 +7,117 @@ using System.Windows;
 using System.Reflection;
 using System.Diagnostics;
 
+using Microsoft.Extensions.CommandLineUtils;
+
 using NLog;
 
 using PGReliefMoreForGit.Models.Update;
 
 namespace PGReliefMoreForGit
 {
-	/// <summary>
-	/// App.xaml の相互作用ロジック
-	/// </summary>
-	public partial class App : Application
-	{
-		static Logger logger = LogManager.GetCurrentClassLogger();
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
 
-		[STAThread()]
-		public static void Main()
-		{
-			// タイトルを設定
-			var fvi = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
-			var assm = Assembly.GetExecutingAssembly();
-			var name = assm.GetName();
-			logger.Info($"----- Start Application ----- {name.Name} [{fvi.ProductVersion} ({name.Version})] -----");
+        [STAThread()]
+        public static void Main(string[] args)
+        {
+            // タイトルを設定
+            var fvi = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            var assm = Assembly.GetExecutingAssembly();
+            var name = assm.GetName();
+            logger.Info($"----- Start Application ----- {name.Name} [{fvi.ProductVersion} ({name.Version})] -----");
 
-			// アップデート後の起動の場合は後始末を行う
-			Update.Instance.CleanUp();
+            // プログラム引数の互換性確保のための処理
+            EnsureCompatibility(args);
 
-			App app = new App();
-			app.InitializeComponent();
-			app.Run();
-		}
-	}
+
+            // プログラム引数の解析
+            var cla = new CommandLineApplication(throwOnUnexpectedArg: false)
+            {
+                // アプリケーション名（ヘルプの出力で使用される）
+                Name = name.Name,
+            };
+
+            // ヘルプ出力のトリガーとなるオプションを指定
+            cla.HelpOption("-?|-h|--help");
+
+            // 読み込むファイル
+            var inputfileOptions = cla.Option("-f|--file <filename>", "Read a setting file.", CommandOptionType.SingleValue);
+
+            // CLI モード
+            cla.Command("cli", command =>
+            {
+                // 説明（ヘルプの出力で使用される）
+                command.Description = "Command Line Interface Mode (No GUI)";
+
+                command.HelpOption("-?|-h|--help");
+
+                command.OnExecute(() =>
+                {
+                    Console.WriteLine("Hello World!");
+                    return 0;
+                });
+            });
+
+            // アップデート後のクリーンアップ処理
+            cla.Command("up", command =>
+            {
+                // 説明（ヘルプの出力で使用される）
+                command.Description = "Cleanup after update.";
+
+                command.HelpOption("-?|-h|--help");
+
+                // アップデート前のプロセスID
+                var pidOptions = command.Option("--pid", "Process ID", CommandOptionType.SingleValue);
+
+                // 削除するファイル
+                var deleteFileOptions = command.Option("--file", "Delete files", CommandOptionType.MultipleValue);
+
+                command.OnExecute(() =>
+                {
+                    Update.Instance.CleanUp(Convert.ToInt32(pidOptions.Value()), deleteFileOptions.Values);
+                    Console.WriteLine("cleanup!");
+                    return 0;
+                });
+            });
+
+            // デフォルトの動作
+            cla.OnExecute(() =>
+            {
+                // GUI を起動
+                App app = new App();
+                app.InitializeComponent();
+                return app.Run();
+            });
+
+            cla.Execute(args);
+        }
+
+        static void EnsureCompatibility(string[] args)
+        {
+            // プロセス ID 置換
+            int indexOfUp = Array.IndexOf(args, "/up");
+            if (indexOfUp != -1)
+            {
+                args[indexOfUp] = "up";
+                // プログラム引数 /up の次がプロセスID
+                args[indexOfUp + 1] = "--pid=" + args[indexOfUp + 1];
+            }
+
+            // 削除するファイル置換
+            int indexOfDeleteFiles = Array.IndexOf(args, "/deleteFiles");
+            if (indexOfDeleteFiles != -1)
+            {
+                for (int i = indexOfDeleteFiles + 1; i < args.Length; i++)
+                {
+                    args[i] = "--file=" + args[i];
+                }
+            }
+        }
+    }
 }
 
